d7b81fa5678e505d0bf77bd9c9084a3ada711dfd
Carlos Josiel Hernandez Sanchez <41494301+DeveloperCS@users.noreply.github.com>
2019-05-29 05:32:45 -0500

    añadir editar eq. computo

--- js/funciones/equipos.js
+++ js/funciones/equipos.js
@@ -1,25 +1,25 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyAE5jJKOvZCVZvjFkO0NUBwgvLz4KxnNWk",
-    authDomain: "systemstec-69d04.firebaseapp.com",
-    projectId: "systemstec-69d04",
-    databaseURL: "https://systemstec-69d04.firebaseio.com/"
-  })
-  var db = firebase.database();
+  apiKey: "AIzaSyAE5jJKOvZCVZvjFkO0NUBwgvLz4KxnNWk",
+  authDomain: "systemstec-69d04.firebaseapp.com",
+  projectId: "systemstec-69d04",
+  databaseURL: "https://systemstec-69d04.firebaseio.com/"
+})
+var db = firebase.database();
 
-  /** agregar nueva */
+/** agregar nueva */
 var form = document.getElementById('equiposForm'); // Obtenemos la referencia al formulario
 if (form) { // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
   form.addEventListener('submit', partidaForm1); // Al momento de enviar el formulario, ejecuta la función "contactform"
-   //para la validacion de solo numeros
+  //para la validacion de solo numeros
 }
 $('#modalNuevo1').on('hidden.bs.modal', function (e) {
   form.reset();
-  
+
 })
 
 function partidaForm1(event) {
   event.preventDefault(); // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
- 
+
   const articulo = document.getElementById('articulo');
   const departamento = document.getElementById('departamento');
   const encargado = document.getElementById('encargado');
@@ -38,78 +38,78 @@ function partidaForm1(event) {
     'nMac': nMac.value,
     'nIp': nIp.value
   };
-  insertarEquipos(data); 
+  insertarEquipos(data);
   form.reset(); // borramos todos los campos. 
 }
 
 function insertarEquipos(data) {
 
 
-    db.ref('equipos').push(data)
-      .then(function () {
-        $('#modalNuevo1').modal('hide')
-        // alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
-      })
-      .catch(function () {
-        // alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
-      });
-    llenarTabla();
-  
-  };
+  db.ref('equipos').push(data)
+    .then(function () {
+      $('#modalNuevo1').modal('hide')
+      // alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+    })
+    .catch(function () {
+      // alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+    });
+  llenarTabla();
 
-  function llenarTabla() {
+};
 
-    var data = db.ref('equipos');
-    var table = document.getElementById('contentTable');
-  
-    if (table) {
-  
-      table.innerHTML = "";
-       
-      data.orderByChild("articulo").on("child_added", function (datos) {
-  
-        var d = datos.val();
-     
-        {
-          var row = table.insertRow(0);
-          var cellArticulo = row.insertCell(0);
-          var cellDpto = row.insertCell(1);
-          var cellEncargado = row.insertCell(2);
-          var cellDescripcion = row.insertCell(3);
-        
-          var cellNSerie = row.insertCell(4);
-          var cellNMac = row.insertCell(5);
-          var cellNIp = row.insertCell(6);
+function llenarTabla() {
 
-          var cellEdit = row.insertCell(7);
-          var cellDelete = row.insertCell(8);
-  
-          cellArticulo.innerHTML = d.articulo;
-          cellDpto.innerHTML = d.departamento;
-          cellDescripcion.innerHTML = d.descripcion;
-          cellEncargado.innerHTML = d.encargado;
-          cellNSerie.innerHTML = d.nSerie;
-          cellNMac.innerHTML = d.nMac;
-          cellNIp.innerHTML = d.nIp;
+  var data = db.ref('equipos');
+  var table = document.getElementById('contentTable');
 
-          cellEdit.innerHTML = '<a class="btn btn-warning"><em class="fa fa-edit" style="color:white"></em></a>';
-          cellDelete.innerHTML = ' <a class="btn btn-danger" onclick="remover(\'' + datos.key + '\')"><em class="fa fa-trash-alt" style="color:white"></em></a>';
-  
-        }
-  
-      })
-  
-    }
+  if (table) {
+
+    table.innerHTML = "";
+
+    data.orderByChild("articulo").on("child_added", function (datos) {
+
+      var d = datos.val();
+
+      {
+        var row = table.insertRow(0);
+        var cellArticulo = row.insertCell(0);
+        var cellDpto = row.insertCell(1);
+        var cellEncargado = row.insertCell(2);
+        var cellDescripcion = row.insertCell(3);
+
+        var cellNSerie = row.insertCell(4);
+        var cellNMac = row.insertCell(5);
+        var cellNIp = row.insertCell(6);
+
+        var cellEdit = row.insertCell(7);
+        var cellDelete = row.insertCell(8);
+
+        cellArticulo.innerHTML = d.articulo;
+        cellDpto.innerHTML = d.departamento;
+        cellDescripcion.innerHTML = d.descripcion;
+        cellEncargado.innerHTML = d.encargado;
+        cellNSerie.innerHTML = d.nSerie;
+        cellNMac.innerHTML = d.nMac;
+        cellNIp.innerHTML = d.nIp;
+
+        cellEdit.innerHTML = '<a class="btn btn-warning" onclick="edit(\'' + datos.key + '\')"><em class="fa fa-edit" style="color:white"></em></a>';
+        cellDelete.innerHTML = ' <a class="btn btn-danger" onclick="remover(\'' + datos.key + '\')"><em class="fa fa-trash-alt" style="color:white"></em></a>';
+
+      }
+
+    })
+
   }
-  this.llenarTabla();
+}
+this.llenarTabla();
 
-  function remover(key) {
-    var data = db.ref('equipos');
-    data.child(key).remove();
-    this.llenarTabla()
-  }
+function remover(key) {
+  var data = db.ref('equipos');
+  data.child(key).remove();
+  this.llenarTabla()
+}
 
-  
+
 
 function onKeyUp(event) {
   var keycode = event.keyCode;
@@ -144,7 +144,7 @@ function buscar(value) {
         var cellDpto = row.insertCell(1);
         var cellEncargado = row.insertCell(2);
         var cellDescripcion = row.insertCell(3);
-      
+
         var cellNSerie = row.insertCell(4);
         var cellNMac = row.insertCell(5);
         var cellNIp = row.insertCell(6);
@@ -160,7 +160,7 @@ function buscar(value) {
         cellNMac.innerHTML = d.nMac;
         cellNIp.innerHTML = d.nIp;
 
-        cellEdit.innerHTML = '<a class="btn btn-warning"><em class="fa fa-edit" style="color:white"></em></a>';
+        cellEdit.innerHTML = '<a class="btn btn-warning" onclick="edit(\'' + datos.key + '\')"><em class="fa fa-edit" style="color:white"></em></a>';
         cellDelete.innerHTML = ' <a class="btn btn-danger" onclick="remover(\'' + datos.key + '\')"><em class="fa fa-trash-alt" style="color:white"></em></a>';
 
       }
@@ -169,6 +169,83 @@ function buscar(value) {
   }
 }
 
+//editar
+
+let keyG ='';
+
+function edit(key) {
+  var data = db.ref('equipos');
+  data.orderByChild("articulo").on("child_added", function (datos) {
+    var d = datos.val();
+
+    {
+      if (key == datos.key) {
+        document.getElementById('idEd').value = datos.key;
+        document.getElementById('articuloEd').value = d.articulo;
+        document.getElementById('departamentoEd').value = d.departamento;
+        document.getElementById('encargadoEd').value = d.encargado;
+        document.getElementById('descripcionEd').value = d.descripcion;
+        document.getElementById("nSerieEd").value = d.nSerie;
+        document.getElementById("nMacEd").value = d.nMac;
+        document.getElementById("nIpEd").value = d.nIp;
+
+
+      }
+      
+    }
+  });
+  $('#modalNuevo2').modal('show')
+
+}
+
+var form = document.getElementById('equiposForm1'); // Obtenemos la referencia al formulario
+if (form) { // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+  form.addEventListener('submit', editar); // Al momento de enviar el formulario, ejecuta la función "contactform"
+  //para la validacion de solo numeros
+}
+$('#modalNuevo2').on('hidden.bs.modal', function (e) {
+  form.reset();
+
+})
+
+function editar(event){
+  event.preventDefault(); // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
+const key = document.getElementById('idEd');
+  const articulo = document.getElementById('articuloEd');
+  const departamento = document.getElementById('departamentoEd');
+  const encargado = document.getElementById('encargadoEd');
+  const descripcion = document.getElementById('descripcionEd');
+  const nSerie = document.getElementById("nSerieEd");
+  const nMac = document.getElementById("nMacEd");
+  const nIp = document.getElementById("nIpEd");
+
+  const data = {
+    departamento: departamento.value,
+    articulo: articulo.value,
+    encargado: encargado.value,
+    descripcion: descripcion.value,
+    nSerie: nSerie.value,
+    nMac: nMac.value,
+    nIp: nIp.value
+  };
+  update2(key.value,departamento.value,articulo.value,encargado.value,descripcion.value,nSerie.value,nMac.value,nIp.value);
+}
+
+function update2(key,departamento,articulo,encargado,descripcion,nSerie,nMac,nIp) {
+  var data = db.ref('equipos');
+  data.child(key)
+    .update({
+      departamento: departamento,
+      articulo: articulo,
+      encargado: encargado,
+      descripcion: descripcion,
+      nSerie: nSerie,
+      nMac: nMac,
+      nIp: nIp
+    });
+    $('#modalNuevo2').modal('hide')
+    this.llenarTabla();
+}
 
 function descargarExcel() {
   //Creamos un Elemento Temporal en forma de enlace
